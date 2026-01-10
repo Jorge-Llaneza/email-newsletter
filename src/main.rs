fd6ce105c366a2323aa6d411bda331c99ba64dff
jorge <llanezarodriguezjorge@gmail.com>
2026-01-10 12:22:47 +0100

    added sql testing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::net::TcpListener;
 use email_newsletter;
+use email_newsletter::configurations::get_configuration;
+use std::net::TcpListener;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-        let listener = TcpListener::bind("127.0.0.1:0")?;
-        let server = email_newsletter::startup::run(listener)?;
-        Ok(())
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    let server = email_newsletter::startup::run(listener)?;
+    Ok(())
 }
