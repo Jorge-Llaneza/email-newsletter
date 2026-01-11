e21ce389a8eaca1c3a628daa6154d71004a09b96
jorge <llanezarodriguezjorge@gmail.com>
2026-01-11 13:59:14 +0100

    format

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -68,7 +68,6 @@ async fn subscribe_returns_a_400_for_invalid_form_data() {
 }
 
 async fn spawn_app() -> TestApp {
-
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
