4ee670a2e79a78c72f544b4698330efbfe095c26
jorge <llanezarodriguezjorge@gmail.com>
2026-01-12 16:28:39 +0100

    refactor: cleanup telemetry setup

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,12 @@ use email_newsletter;
 use email_newsletter::configurations::get_configuration;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::{EnvFilter, Registry};
+mod telemetry;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer =
-        BunyanFormattingLayer::new("email-newsletter".to_string(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into());
+    telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let db_pool = PgPool::connect(&configuration.database.connection_string())
