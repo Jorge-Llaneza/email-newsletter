4d418578317899faa5b3fa48aa1bf4e49f53bd07
jorge <llanezarodriguezjorge@gmail.com>
2026-01-12 14:05:41 +0100

    added tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -8,14 +8,21 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     #[cfg(debug_assertions)] env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "adding a new subscriber",
+        %request_id,
+        subscriber_email = form.email,
+        subscriber_name = form.name
+    );
+    let _request_span_guard = request_span.enter();
 
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber",
         request_id,
         form.name,
         form.email
     );
-    log::info!(
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -33,11 +40,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("request_id {} New subscriber details have been saved", request_id);
+            tracing::info!("request_id {} New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("request_id {} Failed to execute query: {:?}",request_id, e);
+            tracing::error!("request_id {} Failed to execute query: {:?}",request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
