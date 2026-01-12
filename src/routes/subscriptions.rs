abb01fe35f36df5f286c75b3e53ec08949cdce2c
jorge <llanezarodriguezjorge@gmail.com>
2026-01-12 16:23:00 +0100

    fix tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,15 +1,13 @@
 use actix_web::{HttpResponse, web};
 use env_logger::Env;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    //TODO if this fn is called more than once it will crash due to the logger init
-    #[cfg(debug_assertions)] env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
     let request_id = Uuid::new_v4();
     let request_span = tracing::info_span!(
-        "adding a new subscriber",
+        "Adding a new subscriber",
         %request_id,
         subscriber_email = form.email,
         subscriber_name = form.name
@@ -22,29 +20,30 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.name,
         form.email
     );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database",);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        uuid::Uuid::new_v4(),
+        Uuid::new_v4(),
         form.email,
         form.name,
         chrono::Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            tracing::info!("request_id {} New subscriber details have been saved", request_id);
+            tracing::info!(
+                "request_id {} New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            tracing::error!("request_id {} Failed to execute query: {:?}",request_id, e);
+            tracing::error!("request_id {} Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
